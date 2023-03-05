c708b3d0913c0a88362d85ec71762337e85d41a5
Moisés Carvalho <moiseslcar@gmail.com>
2023-03-05 09:49:50 -0300

    Commit one

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Criando as imagens..........."
+
+docker build -t moiseslcar/projeto-backend:1.0 backend/.
+docker build -t moiseslcar/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens......."
+
+docker push moiseslcar/projeto-backend:1.0
+docker push moiseslcar/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes...."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments...."
+
+kubectl apply -f ./deployment.yml
